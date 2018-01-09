789bf72c1b2487fad38f319ec6ae0751853961dd
Rubens Figueiredo <rubens.figueiredo@bisdn.de>
2018-01-09 20:35:11 +0100

    fixes

---
+++ netconf/sysrepo/models/install.sh
@@ -0,0 +1,5 @@
+sysrepoctl --install --yang=/home/user/ietf-routing-types.yang --owner=admin:admin --permissions=644
+
+sysrepoctl --install --yang=/home/user/ietf-interfaces.yang --owner=admin:admin --permissions=644
+
+
